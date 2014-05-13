45b69c3001e0bcc3263338f20b2782fe8a9334f6
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-05-13 12:09:23 -0400

    Code style cleanup

--- map.js
+++ map.js
@@ -20,24 +20,19 @@ var svg = d3.select(element)
 var color = d3.scale.linear()
     .range(["#e4efd3", "#c2e699", "#78c679", "#31a354", "#006837"]);
 
-var rankColors = ["#2ecc71", "#3498db", "#9b59b6", "#e74c3c", "#e67e22", "#f1c40f", "#c0392b", "#2980b9", "#27ae60", "#f39c12", "#d35400"];
+var rankColors = ["#2ecc71", "#3498db", "#9b59b6", "#e74c3c", "#e67e22", "#f1c40f",
+                  "#c0392b", "#2980b9", "#27ae60", "#f39c12", "#d35400"];
 var assignedRankColors = {};
 var rankColorsIndex = 0;
 
 // Create JSON object
 var json = JSON.parse(AtlasJSONData);
 
-/* Code to display text directly on map, if needed
-svg.append("text")
-    .attr("transform", "translate(" + 20 + "," + 350 + ")")
-    .style("text-anchor", "left")
-    .text(function(d) { return json.statistics[0].title; });
-*/
-
 // Dynamically add dropdown options from JSON object
 for (var s in json.statistics) {
     if ($.isNumeric(s)) {
-        $(".worldmap-selector", element).append('<option value="' + s + '">' + json.statistics[s].title + '</option>');
+        $(".worldmap-selector", element)
+            .append('<option value="' + s + '">' + json.statistics[s].title + '</option>');
     }
 }
 
@@ -62,16 +57,6 @@ function selection(choice) {
 
     var type = json.statistics[choice].type; // Used to store type (slider, rank, etc.)
 
-    // Update selection title
-    // $('.worldmap-title', element).text(json.statistics[choice].title);
-
-    /* Code to update text displayed directly on map, if needed
-    svg.selectAll("text")
-        .attr("class", "title")
-        .style("text-anchor", "left")
-        .text(function(d) { return json.statistics[choice].title; });
-    */
-
     var overall = "Global: ";
     if (type == "mrq" || type == "slider") {
         overall += round(json.statistics[choice].average);
@@ -86,9 +71,11 @@ function selection(choice) {
         .style("fill", function(d) {
             if (d.properties.name) // Ignore undefined
             {
-                var countryValue = json.statistics[choice].countries[d.properties.name]; // Get country value from JSON
+                // Get country value from JSON
+                var countryValue = json.statistics[choice].countries[d.properties.name];
                 if (countryValue !== undefined && type != "rank") {
-                    return color(countryValue); // Return corresponding color depending on country value
+                    // Return corresponding color depending on country value
+                    return color(countryValue);
                 } else if (countryValue !== undefined && type == "rank") {
                     // Set new specific color for answer, if not yet encountered
                     if (!assignedRankColors[countryValue]) {
@@ -114,7 +101,8 @@ function mouseover() {
 function mousemove(d) {
     if (d.properties.name) {
         var type = json.statistics[choiceElement].type;
-        var countryValue = json.statistics[choiceElement].countries[d.properties.name]; // Pull unique country value
+        // Pull unique country value
+        var countryValue = json.statistics[choiceElement].countries[d.properties.name];
 
         var detailString;
         if (countryValue === undefined) {
@@ -141,7 +129,8 @@ function mouseout() {
 
 // Main D3.js function
 d3.json("world.json", function(error, world) {
-    countries = topojson.feature(world, world.objects.countries).features; // Load TopoJSON object
+    // Load TopoJSON object
+    countries = topojson.feature(world, world.objects.countries).features;
 
     svg.selectAll(".country")
         .data(countries)
